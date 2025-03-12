bc5232914611750f3cd889819abcebbc872ef977
Dave Bush <davebush@gmail.com>
2025-03-12 11:59:38 -0400

    Fixed todos e2e tests
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- apis/apps/todos/src/main.ts
+++ apis/apps/todos/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import { nestInit } from "./nest-init";
 
 async function bootstrap() {
+  Logger.log(`CWD: ${process.cwd()}`)
   const app = await nestInit();
   const port = process.env.PORT ?? 3000
   Logger.log(`Server started on port: ${port}`, "Main");
